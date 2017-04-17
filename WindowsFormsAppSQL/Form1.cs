1d3e5c06bbb32929e17509df9cf9bd976c710e18
beisenbek.baisakov <beisenbek.baisakov@yandex.ru>
2017-04-17 16:32:55 +0600

    init

---
+++ WindowsFormsAppSQL/Form1.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection mySqlConnection;
+        MySqlDataAdapter mySqlDataAdapter;
+        MySqlCommandBuilder mySqlCommandBuilder;
+        DataTable dataTable;
+        BindingSource bindingSource;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mySqlConnection = new MySqlConnection(
+                   "SERVER=localhost;" +
+                   "DATABASE=kbtudb2;" +
+                   "UID=root;" +
+                   "PASSWORD=;");
+
+            mySqlConnection.Open();
+
+            string query = "SELECT * FROM student";
+
+            mySqlDataAdapter = new MySqlDataAdapter(query, mySqlConnection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+
+            mySqlDataAdapter.UpdateCommand = mySqlCommandBuilder.GetUpdateCommand();
+            mySqlDataAdapter.DeleteCommand = mySqlCommandBuilder.GetDeleteCommand();
+            mySqlDataAdapter.InsertCommand = mySqlCommandBuilder.GetInsertCommand();
+
+            dataTable = new DataTable();
+            mySqlDataAdapter.Fill(dataTable);
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = dataTable;
+
+            dataGridView1.DataSource = bindingSource;
+            bindingNavigator1.BindingSource = bindingSource;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mySqlDataAdapter.Update(dataTable);
+        }
+    }
+}
